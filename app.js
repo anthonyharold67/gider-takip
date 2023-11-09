6999bee98c485a0cbbc9a72ae1f353a3302d7774
anthonyharold67 <93030628+anthonyharold67@users.noreply.github.com>
2023-11-09 16:29:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,158 +1,144 @@
-//* böyle tek tek inputları yakalyıp change venti tanımlamak yerine aşağıda yakladığımız formun childNodeslarında gezinip her birine change eventı tanımlamış olduk.
-// const giderText= document.getElementById("giderText");
-// const giderTarih= document.getElementById("giderTarih");
-// const giderMiktar= document.getElementById("giderMiktar");
+//? Selectors
+const ekleBtn = document.getElementById("ekle-btn")
+const gelirInput = document.getElementById("gelir-input")
+const ekleFormu = document.getElementById("ekle-formu")
 
-//? declaration
-const giderForm = document.getElementById("giderForm");//form
-const tableBody = document.getElementById("table-body");//tbody
-const gelirVal = document.getElementById("gelir-val");//gelir bilgisinin girildiği input
-const gelirEkle = document.getElementById("ekle");// gelirin eklendiği button
-const kalanDiv = document.getElementById("kalan");// toplam bilgilerinin gösterildiği alan
-const myDate = document.getElementById("giderTarih");// tarihin seçildiği inputa min ve max dğerlerini tanımlamak için yakaladığımız input.
-// const alertDiv = document.getElementById("alert");// uyarıyı gösterdiğimiz div
-const temizle = document.getElementById("temizle");// bilgilerin hepsini temizlemek için kullanılan buton
+//? Sonuc tablosu
+const gelirinizTd = document.getElementById("geliriniz")
+const giderinizTd = document.getElementById("gideriniz")
+const kalanTd = document.getElementById("kalan")
 
-//! inputun min ve max tarihlerini belirleme
-myDate.max = `${new Date().toLocaleDateString("en-ca").replaceAll(".", "-")}T23:59:00`;//inputun max attribute u değer olarak bekledği tarih formatına uyarladık => 2023-01-20T23:59:00 
+//? harcama formu
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const miktarInput = document.getElementById("miktar")
 
-const month =new Date().getMonth() < 9 ? "0" + (Number(new Date().getMonth()) + 1): (Number(new Date().getMonth()) + 1);//getMonth() metodu 0 - 11 arasında değer döndüğü için burada algoritma kurup içinde bulunduğumuz ayın tek haneli olup olmadığını yakalyıp tek haneliyse başına 0 ekliyoruz ve gelen değere asıl ayı bulmak için 1 ekliyoruz.
+//? Haracama Tablosu
+const harcamaBody = document.getElementById("harcama-body")
+const temizleBtn = document.getElementById("temizle-btn")
 
-myDate.min = `${new Date().getFullYear()}-${month}-01T00:00:00`;//gün ve saat sabit olduğu için ay ve yıl bilgisini dinamik bir şekilde alıyoruz
+//? Variables
+let gelirler = 0
 
-//! gelir bilgisinin eklendği alan
-let gelir = Number(localStorage.getItem("gelir")) || 0;
-gelirEkle.addEventListener("click", () => {
-  gelir = gelir + Number(gelirVal.value);
-  localStorage.setItem("gelir", gelir);
-  gelirVal.value = "";
-  kalanHesapla();
-});
+//? tum harcamalari saklayacak dizi (JSON)
+let harcamaListesi = []
 
-//! formdaki bilgileri toplamak için bir obje tanımladık. Ve key isimleriyle inputlardaki name attirubutelerindeki isimler ile aynı tanımladık.
-let initialState = {
-  id: "",
-  yer: "",
-  tarih: "",
-  miktar: "",
-};
+//?Events
 
-//! harcamaları tutacağımız arrayi oluşturduk 
-let myArr = [];
+//! Formun submit butonuna basildiginda
+ekleFormu.addEventListener("submit", (e) => {
+  e.preventDefault() //? reload'u engeller
+  gelirler = gelirler + Number(gelirInput.value) //? string eklemiyi engelledik
 
-//! load işlemi olduğunda localden bilgileri çekmek için
+  //? gelirlerin kalıcı olmasi icin localStorage a kopyaliyoruz
+  localStorage.setItem("gelirler", gelirler)
+
+  //? input degerini sifrladik
+  ekleFormu.reset()
+
+  //? Degisiklikleri sonuc tablosuna yazan fonks.
+  hesaplaVeGuncelle()
+})
+
+//! Sayfa her yuklendikten sonra calisan event
 window.addEventListener("load", () => {
-  myArr = JSON.parse(localStorage.getItem("harcamalar")) || [];
-  if (myArr.length > 0) {
-    myArr.forEach(item => getCreateElem(item));
+  //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
+  gelirler = Number(localStorage.getItem("gelirler"))
+
+  //? localStroge'den harcama listesini okuyarak global dizimize saklıyoruz.
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || []
+
+  //? harcama dizisinin icindeki objleri tek tek DOMa yaziyoruz.
+  harcamaListesi.forEach((harcama) => harcamayiDomaYaz(harcama))
+
+  console.log(harcamaListesi)
+  //? Tarih inputunu bugun deger ile yukle
+  tarihInput.valueAsDate = new Date()
+
+  //? Degisen bilgileri hesapla ve DOM'a bas
+  hesaplaVeGuncelle()
+})
+
+//! harcama formu submit edildiginde calisir
+harcamaFormu.addEventListener("submit", (e) => {
+  e.preventDefault() //? reload'u engelle
+
+  //? yeni harcama bilgileri ile bir obje olusutur
+  const yeniHarcama = {
+    id: new Date().getTime(), //? Sistem saatini (ms olarak) verir. Unique gibidir.
+    tarih: tarihInput.value,
+    alan: harcamaAlaniInput.value,
+    miktar: miktarInput.value,
   }
-  kalanHesapla();//başlangıçta toplam bilgilerinin gösterilmesi için çağırıyoruz
-});
 
-//! formdaki bilgilerin change eventi tetiklendiğinde initialState e atmak için tanımladığımız fonksiyon
-const handleChange = (e) => {
-  e.preventDefault();
-  initialState = { ...initialState, [e.target.name]: e.target.value };//name attributelarından gelen değer ile initialState objesinde bulunan değer aynı olduğu için ilgili keyi bracket notation la güncellemiş oluyoruz. yani e.targetın name inden yer değeri geliyor.gelen değer string olduğu için bracket notation kullanıyoruz.["yer"]:"market" vb.gibi
-};
+  //? yeni harcama objesini diziye ekle
+  harcamaListesi.push(yeniHarcama)
 
-//! formun submit işleminin gerçekleştiği alan
-giderForm.addEventListener("submit", (e) => {
-  e.preventDefault();// formun kendine ait özelliklerini durduruyoruz.
-  initialState = { ...initialState, id: new Date().getTime() };//oluşturğumuz initialState objesininin id değerini güncelliyoruz. Silme işleminde kullanacağız
-  myArr.push(initialState);// arrayimize veriyi ekliyoruz
-  localStorage.setItem("harcamalar", JSON.stringify(myArr));//arrayi localStorage a ekliyoruz
-  getCreateElem(initialState);//htmle eklemek için fonksiyona objemizi parametre olarak gönderiyoruz
-  kalanHesapla();// arrayimiz güncellendiği için toplam bilgilerinin de güncellenmesi gerekiyor
-  initialState = {
-    id: "",
-    yer: "",
-    tarih: "",
-    miktar: "",
-  };// initialstate objesini başlangıç haline döndürüyoruz
-  giderForm.reset();//formu boşaltıyoruz
-});
+  //? dizisin son halini localStorage e gonder.
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi))
 
-//! harcamalar tablosunu oluşturduğumuz kısım
-function getCreateElem(item, i) {
-  const newElem = `
-       <td>${new Date(item.tarih).toLocaleString("tr-TR")}</td>
-      <td class="text-capitalize">${item.yer}</td>
-      <td>${item.miktar} ₺</td>
-      <td class="silme">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" role="button" class="bi bi-trash3" viewBox="0 0 16 16">
-          <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-        </svg>
-    </td>`
-    ;
-  const tr = document.createElement("tr");
-  tr.setAttribute("id", item.id);// objeye verdiğimiz id tr elementinin id bilgisine tanımlamış olduk
-  tr.innerHTML = newElem;
-  tableBody.prepend(tr);//başa eklemek için prepend kullandık
+  harcamayiDomaYaz(yeniHarcama)
+
+  hesaplaVeGuncelle()
+
+  //? Formdaki verileri sil
+  harcamaFormu.reset()
+  tarihInput.valueAsDate = new Date()
+})
+
+const hesaplaVeGuncelle = () => {
+  const giderler = harcamaListesi.reduce(
+    (toplam, harcama) => toplam + Number(harcama.miktar),
+    0
+  )
+
+  gelirinizTd.innerText = gelirler
+  giderinizTd.innerText = giderler
+  kalanTd.innerText = gelirler - giderler
 }
 
-//! silme işlemini gerçekleştirdiğimiz kısım.
-tableBody.addEventListener("click", (e) => {
-  if (e.target.classList.contains("bi-trash3")) {
-    const id = e.target.parentElement.parentElement.getAttribute("id"); //içinde bulunduğu tr elementinin id sini yakaladık
-    e.target.parentElement.parentElement.remove(); //Uı den sildik
-    myArr = myArr.filter((gider) => gider.id != id); // arrayden silmiş olduk
-    localStorage.setItem("harcamalar", JSON.stringify(myArr)); //locali update etmiş olduk
-    kalanHesapla(); // total bilgilerini güncellemiş olduk
-//     let myAlert = `
-//       <div
-//         class="alert alert-success w-50 d-flex justify-content-between"
-//         role="alert">
-//         <span>Harcamayı başarıyla sildiniz!</span>
-//         <button
-//           type="button"
-//           class="btn-close"
-//           data-bs-dismiss="alert"
-//           aria-label="Close"></button>
-//       </div>
-//     `; //alert verdirmek için
-//     alertDiv.innerHTML = myAlert;
-//     setTimeout(() => {
-//       alertDiv.innerHTML = "";
-//     }, 3000); //alerti 3 saniye sonra kaldırmak için
-  }
-});
-
-//! change evntlarını tanımladığımız alan
-giderForm.childNodes.forEach((elem) =>
-  elem.addEventListener("change", handleChange)
-);//form elemntinin childNodeslarında gezinerek her birine change eventı tanımlamış olduk.Aşağıdaki gibi tek tek uğraşmadık
-
-// giderText.addEventListener("change",handleChange);
-
-// giderTarih.addEventListener("change",handleChange);
-
-// giderMiktar.addEventListener("change",handleChange);
-
-
-//! toplam bilgilerinin hesaplandığı alan
-function kalanHesapla() {
-  const totalHarcama = myArr.reduce((acc, har) => acc + Number(har.miktar), 0);
-  const kalanMiktar = gelir - totalHarcama;
-  kalanDiv.innerHTML = `
+const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
+  // const { id, miktar, tarih, alan } = yeniHarcama
+  harcamaBody.innerHTML += `
   <tr>
-  <th><span>Geliriniz</span></th>
-  <td><span>: ${gelir} ₺ </span></td>
+    <td>${tarih}</td>
+    <td>${alan}</td>
+    <td>${miktar}</td>
+    <td><i id=${id} class="fa-solid fa-trash-can text-danger"  type="button"></i></td>
   </tr>
-  <tr>
-  <th><span>Gideriniz</span></th>
-  <td><span>: ${totalHarcama} ₺ </span></td>
-  </tr>
-  <tr>
-  <th><span>Kalan</span></th>
-  <td><span>: ${kalanMiktar} ₺ </span></td>
-    `;
+  `
 }
+//! Harcama tablosunda herhangi bir alana tiklanildiginda calisir.
+harcamaBody.addEventListener("click", (e) => {
+  // console.log(e.target)
 
-//! bütün bilgilerin temizlendği alan
-temizle.addEventListener("click",()=>{
-  myArr = [];
-   gelir = 0;
-  localStorage.clear();
-  kalanHesapla();
-  tableBody.innerHTML="";
+  //? Tiklama sil butonlarindan geldi ise
+  if (e.target.classList.contains("fa-trash-can")) {
+    //? DOM'dan ilgili row'u sildik.
+    e.target.parentElement.parentElement.remove()
+
+    const id = e.target.id
+    console.log(id)
+
+    //? Dizideki ilgili objeyi sildik.
+    harcamaListesi = harcamaListesi.filter((harcama) => harcama.id != id)
+
+    //? Silinmis yeni diziyi Local Storage aktardik.
+    localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi))
+
+    //? her satir silindikten sonra yeni degerleri hesapla ve DOM'a yaz
+    hesaplaVeGuncelle()
+  }
+})
+
+//? temizle butonına basildigi zaman calis
+temizleBtn.addEventListener("click", () => {
+  if (confirm("Silmek istedigine emin misiniz?")) {
+    harcamaListesi = [] //? RAM'deki harcama listesini sil
+    gelirler = 0 //? RAM'deki gelirleri sil
+    localStorage.clear() //? local straoge'daki tüm verileri sil
+    harcamaBody.innerHTML = "" //? DOM'daki tüm harcamlar sil
+    hesaplaVeGuncelle() //? sonuc tablosundaki (DOM) gelirler, giderler ve kalan degerleri sil.
+  }
 })
